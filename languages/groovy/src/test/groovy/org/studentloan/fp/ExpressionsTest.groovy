57c53a449e58e40227b89b2ead0b96f0e45b4229
pair9 <pair9@studentloan.org>
2014-04-28 18:07:41 -0500

    Brian: Adding presentation, notes and groovy project.

---
+++ languages/groovy/src/test/groovy/org/studentloan/fp/ExpressionsTest.groovy
@@ -0,0 +1,14 @@
+package org.studentloan.fp
+
+import org.junit.Test
+
+public class ExpressionsTest {
+  @Test
+  void testExpression() {
+    tryToReturnAStatement()
+  }
+
+  int tryToReturnAStatement() {
+    int x = 1
+  }
+}
