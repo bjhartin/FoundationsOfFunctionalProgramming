2c68cfd0e30f3e9063bba63f19f3981944c0183d
pair9 <pair9@studentloan.org>
2014-04-28 18:11:29 -0500

    Brian: Adding aggregator project.

--- languages/java/src/test/java/org/studentloan/fp/ExpressionsTest.java
+++ languages/java/src/test/java/org/studentloan/fp/ExpressionsTest.java
@@ -5,11 +5,11 @@ import org.junit.Test;
 public class ExpressionsTest {
     @Test
     public void testExpression(){
-        System.out.println("tryToReturnAStatement() = " + tryToReturnAStatement());
-        assert(1 == 1);
+//        tryToReturnAStatement());
+        assert 1 == 1;
     }
 
-    private int tryToReturnAStatement() {
-        return (int x = 1);
-    }
+//    private int tryToReturnAStatement() {
+//        return (int x = 1);
+//    }
 }
