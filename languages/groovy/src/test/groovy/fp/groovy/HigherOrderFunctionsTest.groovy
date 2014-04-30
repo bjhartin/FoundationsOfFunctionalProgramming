600dc5324ecd22156e66778592d8b61fddc81f07
pair9 <pair9@studentloan.org>
2014-04-30 18:23:06 -0500

    Brian: Removing unnecessary comments.

--- languages/groovy/src/test/groovy/fp/groovy/HigherOrderFunctionsTest.groovy
+++ languages/groovy/src/test/groovy/fp/groovy/HigherOrderFunctionsTest.groovy
@@ -3,48 +3,37 @@ package fp.groovy
 import org.junit.Test
 
 class HigherOrderFunctionsTest {
-  // We'll declare a function that we will later pass as an argument.
   boolean isEven(int number) {
     number % 2 == 0
   }
 
-  // This function returns a function.
   Closure multiplyBy(int multiplier) {
-    // We define a function, on the spot, which takes an Int
-    // and multiplies it by the given multiplier.
     {number ->
       number * multiplier
     }
   }
 
-  // This function takes a function as its second argument.
-  // The 'Int => Boolean' syntax is shorthand for 'Function1[Int, Boolean]'.
   boolean checkNumber(int number, Closure check) {
     check(number)
   }
 
   @Test
   void functionsCanTakeFunctionsAsParameters() {
-    // Passing the function as the second argument.
-    // In order to treat the function as a value, we have to use 'this.&' to convert
-    // it to a closure.  This is clumsy.
+    // In order to treat the function as a value, we have to use 'this.&'.
     assert checkNumber(2, this.&isEven)
     assert !checkNumber(3, this.&isEven)
   }
 
   @Test
   void functionsCanBeAnonymous() {
-    // We pass a function that does not have a name.
-    // Our function checks for odd numbers.
-    assert checkNumber(3, {n -> n % 2 != 0})
+    assert checkNumber(3, {n -> n % 2 != 0})  // Called lambda expressions in most languages
   }
 
   @Test
   void functionsCanReturnFunctions() {
     Closure timesFive = multiplyBy(5)
 
-    assert timesFive instanceof Closure // Yep, it returned a function
-
+    assert timesFive instanceof Closure
     assert 30 == timesFive(6)
   }
 }
--- languages/scala/src/test/scala/fp/scala/HigherOrderFunctionsTest.scala
+++ languages/scala/src/test/scala/fp/scala/HigherOrderFunctionsTest.scala
@@ -4,19 +4,13 @@ import org.junit.Test
 import org.junit.Assert._
 
 class HigherOrderFunctionsTest {
-  // We'll declare a function that we will later pass as an argument.
   def isEven(number: Int): Boolean = {number % 2 == 0}
 
-  // This function returns a function.
   def multiplyBy(multiplier: Int): Int => Int = {
-    // We define a function, on the spot, which takes an Int
-    // and multiplies it by the given multiplier.
     (number:Int) => {
       number * multiplier
     }
   }
-
-  // This function takes a function as its second argument.
   // The 'Int => Boolean' syntax is shorthand for 'Function1[Int, Boolean]'.
   def checkNumber(number: Int, check: Int => Boolean):Boolean = {
     check(number)
@@ -24,24 +18,20 @@ class HigherOrderFunctionsTest {
 
   @Test
   def functionsCanTakeFunctionsAsParameters() {
-    // Passing the function as the second argument.
     assertTrue(checkNumber(2, isEven))
     assertFalse(checkNumber(3, isEven))
   }
 
   @Test
   def functionsCanBeAnonymous() {
-    // We pass a function that does not have a name.
-    // Our function checks for odd numbers.
-    assertTrue(checkNumber(3, (number:Int) => {number % 2 != 0}))
+    assertTrue(checkNumber(3, (number:Int) => {number % 2 != 0}))  // Called lambda expressions in most languages
   }
 
   @Test
   def functionsCanReturnFunctions() {
-    val timesFive = multiplyBy(5) //
+    val timesFive = multiplyBy(5)
 
-    assertTrue(timesFive.isInstanceOf[Int => Int]) // Yep, it returned a function
-
+    assertTrue(timesFive.isInstanceOf[Int => Int])
     assertEquals(30, timesFive(6))
   }
 }
