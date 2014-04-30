914299dbfbce99fdd764fe2fda823f8fe182d103
pair9 <pair9@studentloan.org>
2014-04-30 18:06:50 -0500

    Brian: Added higher order function examples.

---
+++ languages/scala/src/test/scala/fp/scala/HigherOrderFunctionsTest.scala
@@ -0,0 +1,40 @@
+package fp.scala
+
+import org.junit.Test
+import org.junit.Assert._
+
+class HigherOrderFunctionsTest {
+  // We'll declare a function that we will later pass as an argument.
+  def isEven(number: Int): Boolean = {number % 2 == 0}
+
+  // This function returns a function.
+  def multiplyBy(multiplier: Int): Int => Int = {
+    // We define a function, on the spot, which takes an Int
+    // and multiplies it by the given multiplier.
+    (number:Int) => {
+      number * multiplier
+    }
+  }
+
+  // This function takes a function as its second argument.
+  // The 'Int => Boolean' syntax is shorthand for 'Function1[Int, Boolean]'.
+  def checkNumber(number: Int, check: Int => Boolean):Boolean = {
+    check(number)
+  }
+
+  @Test
+  def functionsCanTakeFunctionsAsParameters() {
+    // Passing the function as the second argument.
+    assertTrue(checkNumber(2, isEven))
+    assertFalse(checkNumber(3, isEven))
+  }
+
+  @Test
+  def functionsCanReturnFunctions() {
+    val timesFive = multiplyBy(5) //
+
+    assertTrue(timesFive.isInstanceOf[Int => Int]) // Yep, it returned a function
+
+    assertEquals(30, timesFive(6))
+  }
+}
